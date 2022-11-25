3f6bfb3b6c7a3d77612e59553028c8e168041beb
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2022-11-25 17:52:31 +0800

    chore: change vue editor package

--- src/components/base/mention.vue
+++ src/components/base/mention.vue
@@ -1,12 +1,50 @@
 <template>
   <div>
-    mention
+    <vue-editor
+      id="notes-vue-editor"
+      v-model="vModelProxy"
+      placeholder="Type a message..."
+      :editor-options="{
+        modules: {
+          toolbar: false
+        }
+      }"
+      @input="onInput"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import { VueEditor } from 'vue2-editor'
 
+export default {
+  components: {
+    VueEditor
+  },
+
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    vModelProxy: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        return this.$emit('input', val)
+      }
+    }
+  },
+
+  methods: {
+    onInput() {
+      console.log('asdf')
+    }
+  }
 }
 </script>
 
