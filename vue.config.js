0edff00a8112298eb4ab16254761619c785e527d
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2022-11-25 17:41:28 +0800

    chore: add eslint

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      require('unplugin-auto-import/webpack')({ /* options */ }),
+      require('unplugin-vue-components/webpack')({
+            directoryAsNamespace: false,
+      }),
+    ],
+  },
 })
